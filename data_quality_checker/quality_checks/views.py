7cc849218134c7272ce47702c3c79cf399c64598
ksenijasam <ksenija2702@gmail.com>
2026-03-27 18:46:19 +0100

    initial file_upload logic (.csv); inital null check logic; emty forms.py; checking file_upload.html

--- data_quality_checker/quality_checks/views.py
+++ data_quality_checker/quality_checks/views.py
@@ -1,6 +1,21 @@
-from django.http import HttpResponse, HttpResponseRedirect
+import io
+import pandas as pd
+
 from django.shortcuts import render
-from django.urls import reverse
 
 def file_upload(request):
+    try:
+        if request.method == "POST" and request.FILES.get('csv_file'):
+            csv_file = request.FILES['csv_file']
+
+            data = csv_file.read().decode('utf-8')
+            df = pd.read_csv(io.StringIO(data))
+
+            print(check_for_null_fields(df))
+    except:
+        print('redirect & present the error')  #TODO
+
     return render(request, "file_upload.html", {})
+
+def check_for_null_fields(df):
+    return df.isnull().sum()
