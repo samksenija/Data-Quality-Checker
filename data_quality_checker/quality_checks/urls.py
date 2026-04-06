18837ebb90b5ecda30c38964cd5482f00e315d06
samksenija <ksenijasamardzic4@gmail.com>
2026-04-06 16:56:17 +0200

    login user - migrations, view functions, urls - adding login logic to application

--- data_quality_checker/quality_checks/urls.py
+++ data_quality_checker/quality_checks/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.file_upload, name="index_file_upload"),
+    path("", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
     path("file_upload", views.file_upload, name="file_upload"),
     path("null-value-details", views.null_value_details, name="null_value_details"),
     path("results", views.results, name="results"),
--- data_quality_checker/quality_checks/views.py
+++ data_quality_checker/quality_checks/views.py
@@ -7,7 +7,15 @@ from .forms import ColumnMappingForm
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+ 
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import User
+from django.db import IntegrityError
+
 from .utils import check_for_null_fields_count, check_for_null_fields_index_column, check_for_duplicate_rows, schema_check_datatypes, generate_pdf, results_context 
+
 
 df = None
 duplicate_rows = None
@@ -29,6 +37,59 @@ data_types = {
     "Dictionary": "dict",
     "Null": "none"
 }
+
+      
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("file_upload"))
+        else:
+            return render(request, "login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("login"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+
+        except IntegrityError:
+            return render(request, "register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("file_upload"))
+    else:
+        return render(request, "register.html")
 
 def file_upload(request):
     try:
