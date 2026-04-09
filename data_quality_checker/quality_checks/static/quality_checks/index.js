df1f661275bfe45ebb8845e0ea46cb2fe114ca99
samksenija <ksenijasamardzic4@gmail.com>
2026-04-09 14:34:35 +0200

    setting the basis for archive element deletion

--- data_quality_checker/quality_checks/static/quality_checks/index.js
+++ data_quality_checker/quality_checks/static/quality_checks/index.js
@@ -5,5 +5,24 @@ document.addEventListener('DOMContentLoaded', function() {
         'Content-Type': 'application/json',
         'X-CSRFToken': csrftoken
     };
+});
 
-})
+function delete_archive_element(){
+        alert("Delete PDF"); //TODO
+}
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
